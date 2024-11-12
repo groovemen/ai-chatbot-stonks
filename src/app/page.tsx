5dbe28676c46a7b3e75a8e2e8fc739f10fb4ba33
Stefan <moldovanste@gmail.com>
2024-11-12 19:05:04 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <h1 className="text-4xl text-white">Ai Chatbot Stocks</h1>
+          <h1 className="text-4xl text-white">AI Chatbot Stocks</h1>
           <Button className="absolute bottom-5 right-5" isIconOnly size="lg" color="danger" aria-label="Like">
             <svg
               width="24px"
